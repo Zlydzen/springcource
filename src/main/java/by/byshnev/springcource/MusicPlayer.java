1ae5521458e1271ec0d553493ab32ef36055e7db
Mikita Byshniou <nick.byshnev@gmail.com>
2021-12-02 11:28:09 +0300

    Make collection ijection in Spring

--- src/main/java/by/byshnev/springcource/MusicPlayer.java
+++ src/main/java/by/byshnev/springcource/MusicPlayer.java
@@ -1,11 +1,18 @@
 package by.byshnev.springcource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
 
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
 
     private String name;
     private int volume;
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
 
     public String getName() {
         return name;
@@ -23,20 +30,9 @@ public class MusicPlayer {
         this.volume = volume;
     }
 
-    // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public void playMusicList() {
+        for (Music music : musicList) {
+            System.out.println("Playing " + music.getSong());
+        }
     }
-
-    public MusicPlayer() {
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
-    }
-
 }
